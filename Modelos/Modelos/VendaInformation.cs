592fec80d1d5e8ef027e536b9caca87cfe5c2a13
Marcio de Andrade <marcio@requestdev.net>
2025-04-21 11:10:45 -0400

    Criação da classe VendaInformation

---
+++ Modelos/Modelos/VendaInformation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class VendaInformation
+    {
+        private int _codigo;
+        public int Codigo
+        {
+            get { return _codigo; }
+            set { _codigo = value; }
+        }
+        private DateTime _dataVenda;
+        public DateTime DataVenda
+        {
+            get { return _dataVenda; }
+            set { _dataVenda = value; }
+        }
+        private decimal _valorTotal;
+        public decimal ValorTotal
+        {
+            get { return _valorTotal; }
+            set { _valorTotal = value; }
+        }
+        private List<ProdutoInformation> _produtos;
+        public List<ProdutoInformation> Produtos
+        {
+            get { return _produtos; }
+            set { _produtos = value; }
+        }
+    }
+}
