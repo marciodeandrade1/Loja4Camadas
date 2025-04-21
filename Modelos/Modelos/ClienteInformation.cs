469ca2463923e5d9c2dc662f219bcb1e01d894e3
Marcio de Andrade <marcio@requestdev.net>
2025-04-21 11:06:44 -0400

    Criação da classe ClienteInformation

---
+++ Modelos/Modelos/ClienteInformation.cs
@@ -0,0 +1,30 @@
+﻿namespace Modelos
+{
+    public class ClienteInformation
+    {
+        private int _codigo;
+        public int Codigo
+        {
+            get { return _codigo; }
+            set { _codigo = value; }
+        }
+        private string _nome;
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+        private string _telefone;
+        public string Telefone
+        {
+            get { return _telefone; }
+            set { _telefone = value; }
+        }
+    }
+}
